89ee4a9e8e2c6d24a16a36f9492c9072b06462a5
smartlei <594213482@qq.com>
2017-08-03 22:39:34 +0800

    modified server code

--- ASimpleHttPServer/DealRequest.cs
+++ ASimpleHttPServer/DealRequest.cs
@@ -2,46 +2,55 @@
 using System.Net.Sockets;
 using System.Text;
 
-namespace SimpleHttpServer
+namespace ASimpleHttpServer
 {
     public class DealRequest
     {
-        private string content="";
-
-        byte[] bytes = new byte[1024];
-        public string Sever { get; set; } = "Lave_server";
-        public int StatusCode { get; set; }
-        public Socket Handler { get; set; }
-
         //自定义状态枚举值
         public enum StautucodeString
         {
             OK = 200,
             PasswordWrong = 605,
-            UserWrong=701,
-            AllWrong=810
+            UserWrong = 701,
+            AllWrong = 810
         }
+
+        byte[] bytes = new byte[1024];
+        private string content = "";
+
+        public string Sever { get; set; }
+        public int StatusCode { get; set; }
+        public Socket Handler { get; set; }
 
         //构造函数
         public DealRequest(Socket client)
         {
             Handler = client;
-            client.Receive(bytes);
-            content += Encoding.UTF8.GetString(bytes, 0, bytes.Length);
-            Console.WriteLine("Recive a Http request.");
+            int receiveNum = 1;
+
+            while (receiveNum != 0)
+            {
+                receiveNum = client.Receive(bytes);
+                content += Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                Console.WriteLine("Recive a Http request.");
+            }
+            
 
             //获取参数并判断
-            string user = content.Split(new char[] {' ', '&'}, StringSplitOptions.RemoveEmptyEntries)[1];
-            string password = content.Split(new char[] {' ', '&'}, StringSplitOptions.RemoveEmptyEntries)[2];
-            if (user=="Lave_lei"&&password=="123456")
+            string httpHeader = content.Split(new[] {"\r\n\r\n"}, StringSplitOptions.RemoveEmptyEntries)[0];
+            string httpContent = content.Split(new[] {"\r\n\r\n"}, StringSplitOptions.RemoveEmptyEntries)[1];
+
+            //TODO  将head处理无误后将Content转发至对应的处理函数
+
+            if (httpHeader == "Lave_lei" && httpContent == "123456")
             {
                 StatusCode = 200;
             }
-            else if (password == "123456")
+            else if (httpContent == "123456")
             {
                 StatusCode = 701;
             }
-            else if(user == "Lave_lei")
+            else if (httpHeader == "Lave_lei")
             {
                 StatusCode = 605;
             }
@@ -51,11 +60,12 @@ namespace SimpleHttpServer
             }
         }
 
+
         //构造消息头
         private string BuildHeader()
         {
             StringBuilder header = new StringBuilder("HTTP/1.1");
-            header.Append(" " + StatusCode + " " + ((StautucodeString)StatusCode) + "\r\n");
+            header.Append(" " + StatusCode + " " + ((StautucodeString) StatusCode) + "\r\n");
             header.Append("Content-Type: text/html;\r\ncharset=utf-8\r\n");
             header.Append("Connection: keep-alive\r\n");
             header.Append("Server:Lave_Sever\r\n");
--- ASimpleHttPServer/server.cs
+++ ASimpleHttPServer/server.cs
@@ -1,33 +1,35 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 
-namespace SimpleHttpServer
+namespace ASimpleHttpServer
 {
     internal class Server
     {
-        private bool active = false;
-
+        private bool active;
         //初始化服务器IP和端口
         private Socket server;
         private string serverIP = "127.0.0.1";
-        public int ServerPort { get; } = 6565;
+        private int serverPort = 6565;
+
+        public int ServerPort
+        {
+            get { return serverPort; }
+            set { serverPort = value; }
+        }
 
         //初始化
         public int Initialze()
         {
             if (active)
                 return 0;
-            server = new Socket(SocketType.Stream,ProtocolType.Tcp);
+            server = new Socket(SocketType.Stream, ProtocolType.Tcp);
             //开始监听
-            server.Bind(new IPEndPoint(IPAddress.Parse(serverIP), ServerPort));
+            server.Bind(new IPEndPoint(IPAddress.Parse(serverIP), serverPort));
             server.Listen(10);
             active = true;
-            
+
             Thread activeSever = new Thread(() =>
             {
                 while (active)
@@ -36,10 +38,10 @@ namespace SimpleHttpServer
                     //有新Request时在Console输出其IP
                     Console.WriteLine("A Client Connected in " +
                                       IPAddress.Parse(((IPEndPoint) client.RemoteEndPoint).Address.ToString()));
-                    Thread requestThread = new Thread(() => { ProcessRequst(client); }) { IsBackground = true };
+                    Thread requestThread = new Thread(() => { ProcessRequst(client); }) {IsBackground = true};
                     requestThread.Start();
                 }
-            }){IsBackground = true};
+            }) {IsBackground = true};
             activeSever.Start();
             //初始化成功返回1
             return 1;
@@ -47,16 +49,12 @@ namespace SimpleHttpServer
 
         private void ProcessRequst(Socket client)
         {
-            
             //处理Request
             DealRequest request = new DealRequest(client);
-            if (request.SendResponse() == 1)
+            if (request.SendResponse())
             {
                 client.Close();
             }
         }
-
     }
-
-
 }
